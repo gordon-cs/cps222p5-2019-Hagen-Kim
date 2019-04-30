004ae66a39459fd6b101e0d7ca85c94b606799ab
Jenny Kim <jenny.kim@gordon.edu>
2019-04-30 11:10:06 -0400

    Added a new file for better testing

---
+++ project5.cc
@@ -0,0 +1,16 @@
+#include "graph.cc"
+#include <iostream>
+#include <queue>
+#include <list>
+#include <string>
+#include <map>
+#include <vector>
+
+// Main function
+int main()
+{
+  ifstream file;
+  file.open(file);
+  readFile(ifstream fileInput);
+  print();
+}
